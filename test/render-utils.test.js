0d52e4dad6384e198c5d278f63eb86d267187689
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-02-07 22:55:32 -0800

    test is running

--- test/render-utils.test.js
+++ test/render-utils.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-escape */
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
@@ -8,16 +9,15 @@ const test = QUnit.test;
 test('renderListItem renders a div with animal details', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = `<div class="animal" style="top: 225px; left: 20%;"><img src="./assets/sheep.svg"><span class="name">Samuel the Sheep</span><span>Just because we're sheep, it doesn't mean we have to follow. Think for yourselves.</span></div>`;
-
+    const expected = `<div class=\"animal\"><a href=\"./animals/?id=6\"><img src=\"./assets/sheep.svg\"><span class=\"name\">Samuel the Sheep</span><span>Just because we're sheep, it doesn't mean we have to follow. Think for yourselves.</span></a></div>`;
     //Act
     // Call the function you're testing and set the result to a const
     const samuel = {
+        id: 6,
         name: 'Samuel the Sheep',
         type: 'sheep',
         says: "Just because we're sheep, it doesn't mean we have to follow. Think for yourselves.",
-        top: '225px',
-        left: '20%',
+      
     };
     const actual = renderListItem(samuel);
 
